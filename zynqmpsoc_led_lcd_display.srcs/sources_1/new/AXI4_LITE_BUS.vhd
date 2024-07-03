1be613e3ff5c8a1c717601ed1d25e86238b4bb68
Scott L. McKenzie <72466099+smckenzie-unr@users.noreply.github.com>
2024-07-03 11:24:45 -0500

    Update AXI4_LITE_BUS.vhd
    Having working read operations tested on old Vitis IDE. Will need to test on Vitis unified IDE

--- zynqmpsoc_led_lcd_display.srcs/sources_1/new/AXI4_LITE_BUS.vhd
+++ zynqmpsoc_led_lcd_display.srcs/sources_1/new/AXI4_LITE_BUS.vhd
@@ -1,64 +1,58 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
---use ieee.std_logic_arith.all;
+use ieee.math_real.all;
 
-entity AXI4_LITE_BUS is
-    generic(
-            --AXI Data width: 32 Bits or 64 Bits
-            C_AXI_DATA_WIDTH : positive range 16 to 64 := 32;
-
-            --AXI Address width: 32 Bits or 64 Bits
-            C_AXI_ADDRESS_WIDTH : positive range 16 to 64 := 32;
-
-            --Strobe width
-            C_STROBE_WIDTH : positive := 4;
-
-            --Number or internal registers
-            C_NUM_REGISTERS : positive := 32
-           );
-
+entity AXI4_LITE_SLAVE is
+    generic(C_AXI_ADDRESS_WIDTH : positive range 32 to 128 := 32;
+            C_AXI_DATA_WIDTH : positive range 32 to 128 := 32;
+            C_AXI_STRB_WIDTH : positive range 4 to 8 := 4;
+            C_NUM_REGISTERS : positive range 1 to 4096 := 4);
     port(
-        --Clock and Reset
+         -- Clock and Reset
          ACLK : in std_logic;
          ARESETN : in std_logic;
          
-         --Write Address Channel
+         -- Read Address Channel
+         S_AXI_ARADDR : in std_logic_vector(C_AXI_ADDRESS_WIDTH-1 downto 0);
+         S_AXI_ARVALID : in std_logic;
+         S_AXI_ARREADY : out std_logic;
+         
+         -- Read Data Channel
+         S_AXI_RDATA : out std_logic_vector(C_AXI_DATA_WIDTH-1 downto 0);
+         S_AXI_RRESP : out std_logic_vector(1 downto 0);
+         S_AXI_RVALID : out std_logic;
+         S_AXI_RREADY : in std_logic;
+         
+         -- Write Address Channel
          S_AXI_AWADDR : in std_logic_vector(C_AXI_ADDRESS_WIDTH-1 downto 0);
          S_AXI_AWVALID : in std_logic;
          S_AXI_AWREADY : out std_logic;
 
-         --Write Data Channel
+         -- Write Data Channel
          S_AXI_WDATA : in std_logic_vector(C_AXI_DATA_WIDTH-1 downto 0);
-         S_AXI_WSTRB : in std_logic_vector(C_STROBE_WIDTH-1 downto 0);
+         S_AXI_WSTRB : in std_logic_vector(C_AXI_STRB_WIDTH-1 downto 0);
          S_AXI_WVALID : in std_logic;
          S_AXI_WREADY : out std_logic;
-
-         --Read Address Channel
-         S_AXI_ARADDR : in std_logic_vector(C_AXI_ADDRESS_WIDTH-1 downto 0);
-         S_AXI_ARVALID : in std_logic;
-         S_AXI_ARREADY : out std_logic;
-
-         --Read Data Channel
-         S_AXI_RDATA : out std_logic_vector(C_AXI_DATA_WIDTH-1 downto 0);
-         S_AXI_RVALID : out std_logic;
-         S_AXI_RREADY : in std_logic;
-         S_AXI_RRESP : out std_logic_vector(1 downto 0);
-         
-         --Write Response Channel
+ 
+         -- Write Response Channel
          S_AXI_BRESP : out std_logic_vector(1 downto 0);
          S_AXI_BVALID : out std_logic;
          S_AXI_BREADY : in std_logic
         );
-end AXI4_LITE_BUS;
+end AXI4_LITE_SLAVE;
 
-architecture LOGIC of AXI4_LITE_BUS is
-    --Type array of std_logic_vector used as addressable registers.
-    type register_array is array (C_NUM_REGISTERS downto 0) of std_logic_vector(C_AXI_DATA_WIDTH-1 downto 0);
+architecture LOGIC of AXI4_LITE_SLAVE is
+    constant ADDR_LSB : integer := (C_AXI_DATA_WIDTH/32)+1;
     
-    --Type read state machine
-    type read_state_machine is (IDLE, ADDRESS_LATCH, DATA_OUTPUT);
-
+    constant AXI_RESPONSE_OKAY : std_logic_vector(1 downto 0) := B"00";
+    constant AXI_RESPONSE_EXOKAY : std_logic_vector(1 downto 0) := B"01";
+    constant AXI_RESPONSE_SLVERR : std_logic_vector(1 downto 0) := B"10";
+    constant AXI_RESPONSE_DECERR : std_logic_vector(1 downto 0) := B"11";
+    
+    type register_array is array (C_NUM_REGISTERS downto 0) of std_logic_vector(C_AXI_DATA_WIDTH-1 downto 0);
+    type read_state_machine is (IDLE, READ_ADDR_LATCH, READ_DATA_OUT);
+    
     pure function SLAVE_REG_INIT(array_length : positive) return register_array is
         variable ret_registers : register_array := (others => (others => '0'));
     begin
@@ -68,45 +62,31 @@ architecture LOGIC of AXI4_LITE_BUS is
         return ret_registers;
     end function;
     
-    pure function ADDRESS_INT(stv_address : std_logic_vector(C_AXI_ADDRESS_WIDTH-1 downto 0)) return integer is
-        constant ADDR_LSB  : integer := (C_AXI_DATA_WIDTH/32)+1;
-    begin
-        return to_integer(unsigned(stv_address(stv_address'high downto ADDR_LSB)));
-    end function;
-
-    --constant AXI_RESPONSE_OKAY : std_logic_vector(1 downto 0) := 2X"0";
-    --constant AXI_RESPONSE_EXOKAY : std_logic_vector(1 downto 0) := 2X"1";
-    --constant AXI_RESPONSE_SLVERR : std_logic_vector(1 downto 0) := 2X"2";
-    --constant AXI_RESPONSE_DECERR : std_logic_vector(1 downto 0) := 2X"3";
-
-    signal data_registers : register_array := SLAVE_REG_INIT(C_NUM_REGISTERS); --(others => (others => '0'));
+    signal registers : register_array := SLAVE_REG_INIT(C_NUM_REGISTERS);
     signal read_state : read_state_machine := IDLE;
     
-    signal read_address_reg : natural := 0;
-    signal slv_reg_rden : std_logic := '0';
-
     signal axi_arready : std_logic := '0';
-    signal axi_rdata : std_logic_vector(C_AXI_DATA_WIDTH-1 downto 0) := (others => '0');
     signal axi_rvalid : std_logic := '0';
-    signal axi_rresp : std_logic_vector(1 downto 0) := (others => '0');
-    
     signal axi_awready : std_logic := '0';
-    signal axi_bresp : std_logic_vector(1 downto 0) := (others => '0');
-    signal axi_bvalid : std_logic := '0';
     signal axi_wready : std_logic := '0';
+    signal axi_bvalid : std_logic := '0';
+    
+    signal axi_rresp : std_logic_vector(1 downto 0) := AXI_RESPONSE_SLVERR;
+    signal axi_bresp : std_logic_vector(1 downto 0) := (others => '0');
+    signal axi_rdata : std_logic_vector(C_AXI_DATA_WIDTH-1 downto 0) := (others => '0');
 begin
     S_AXI_ARREADY <= axi_arready;
-    S_AXI_RDATA <= axi_rdata;
     S_AXI_RVALID <= axi_rvalid;
-    S_AXI_RRESP <= axi_rresp;
-    
-    S_AXI_WREADY <= axi_wready;
     S_AXI_AWREADY <= axi_awready;
-    S_AXI_BRESP <= axi_bresp;
+    S_AXI_WREADY <= axi_wready;
     S_AXI_BVALID <= axi_bvalid;
     
+    S_AXI_BRESP <= axi_bresp;
+    S_AXI_RDATA <= axi_rdata;
+    S_AXI_RRESP <= axi_rresp;
     
-    read_state_proc : process(ACLK) is
+    READ_STATE_PROC : process(ACLK) is
+        variable address : natural := 0;
     begin
         if rising_edge(ACLK) then
             if ARESETN = '0' then
@@ -114,25 +94,28 @@ begin
                 axi_arready <= '0';
                 axi_rvalid <= '0';
                 axi_rdata <= (others => '0');
+                axi_rresp <= AXI_RESPONSE_SLVERR;
             else
                 case read_state is
-                    when ADDRESS_LATCH =>
+                    when READ_ADDR_LATCH =>
                         axi_arready <= '1';
-                        read_address_reg <= ADDRESS_INT(S_AXI_ARADDR);
-                        read_state <= DATA_OUTPUT;
-                    when DATA_OUTPUT =>
+                        address := to_integer(unsigned(S_AXI_ARADDR(C_AXI_ADDRESS_WIDTH - 1 downto ADDR_LSB)));
+                        read_state <= READ_DATA_OUT;
+                    when READ_DATA_OUT =>
                         axi_arready <= '0';
                         axi_rvalid <= '1';
-                        axi_rdata <= data_registers(read_address_reg);
+                        axi_rdata <= registers(address);
+                        axi_rresp <= AXI_RESPONSE_OKAY;
                         if S_AXI_RREADY = '1' then
                             read_state <= IDLE;
                         else
-                            read_state <= DATA_OUTPUT;
+                            read_state <= READ_DATA_OUT;
                         end if;
                     when others => --IDLE
                         axi_rvalid <= '0';
+                        axi_rresp <= AXI_RESPONSE_SLVERR;
                         if S_AXI_ARVALID = '1' then
-                            read_state <= ADDRESS_LATCH;
+                            read_state <= READ_ADDR_LATCH;
                         else
                             read_state <= IDLE;
                         end if;
@@ -140,4 +123,5 @@ begin
             end if;
         end if;
     end process;
+    
 end LOGIC;
